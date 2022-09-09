3ec35d14a61bd63eb56b72c0caba8d2243aa1b3b
Robert Adams <adamsiii.robert@gmail.com>
2022-09-09 15:05:50 -0700

    Move questions to own file

---
+++ utils/questions.js
@@ -0,0 +1,59 @@
+const LICENSES = require('./licenses');
+
+module.exports = [
+  {
+    type: 'input',
+    name: 'title',
+    message: 'Enter project title',
+  },
+  {
+    type: 'input',
+    name: 'desc',
+    message: 'Please write a short description of your project',
+  },
+  {
+    type: 'input',
+    name: 'github',
+    message: 'What is your GitHub username?',
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'What is your email address?',
+  },
+  {
+    type: 'input',
+    name: 'install',
+    message: 'What command should be run to install dependencies?',
+    default: 'npm install',
+  },
+  {
+    type: 'input',
+    name: 'test',
+    message: 'What command should be run to run tests?',
+    default: 'npm test',
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'What does the user need to know about using the repo?',
+  },
+  {
+    type: 'input',
+    name: 'contributing',
+    message: 'What does the user need to know about contributing to the repo?',
+    default: 'Contact me to contribute!',
+  },
+  {
+    type: 'list',
+    message: 'What kind of license should this project have?',
+    name: 'license',
+    choices: [
+      LICENSES.MIT,
+      LICENSES.APACHE_2,
+      LICENSES.GPL_3,
+      LICENSES.BSD_3,
+      LICENSES.NONE,
+    ],
+  },
+];
